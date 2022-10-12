f736606aaefef48ed44e02eed9e773540eae26fe
Borwoll <borwoll@borwoll.github.io>
2022-10-12 16:33:47 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>BORWOLL — ОФИЦИАЛЬНАЯ СТРАНИЦА</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container-fluid text-center">
+        <div class="mainBlock" style="margin-top: 15%; text-align: center">
+            <p class="header" style="font-size: 80px; margin-bottom: 0;">BORWOLL</p>
+            <p>Designer & Developer <span id="cursorflash"></span></p>
+        </div>
+    </div>
+</body>
+</html>
